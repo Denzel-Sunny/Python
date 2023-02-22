f752d498d6f16279b4e836c34e48bad14d7a1866
Denzel-Sunny <125428975+Denzel-Sunny@users.noreply.github.com>
2023-02-22 13:45:58 +0530

    Update prg4.py

--- Cycle1/prg4.py
+++ Cycle1/prg4.py
@@ -11,3 +11,27 @@ def word_count(str):
     return counts
 
 print( word_count('the quick brown fox jumps over the lazy dog.'))
+
+
+
+"""
+str = input("Enter the string: ")
+
+str_list = list(str.split(" "))
+print(str_list)
+str_set = list(set(str_list))
+
+for word in str_set:
+    print(word, " = ", str_list.count(word))
+    
+   
+"""
+
+"""string = input("Enter a long string : ")
+search = input("Enter searchble string : ")
+string=string.split(" ")
+count=0
+for i in string:
+    if(i == search):
+        count=count+1
+print(count)"""
